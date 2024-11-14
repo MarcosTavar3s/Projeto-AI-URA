ffec6c4341e530dfb1c30bd206a5369a504e41ff
Marcos Aurélio Tavares Filho <86982304+MarcosTavar3s@users.noreply.github.com>
2024-11-14 18:01:50 -0300

    Update Streamlit.py
    Adição da consulta ao log.txt a cada 2 segundos

--- Dashboard/Streamlit.py
+++ Dashboard/Streamlit.py
@@ -1,20 +1,28 @@
-import streamlit as st
+from threading import Thread
+from streamlit_autorefresh import st_autorefresh
 import paho.mqtt.client as mqtt 
-import time
 import random
+import streamlit as st
+import time
+
+
+file_path='log.txt'
 
 # Classe principal que controla a interface
 class Aplicativo:
     def __init__(self, titulo, mensagem):
         self.titulo = titulo
         self.mensagem = mensagem
+                
+        if 'log' not in st.session_state:
+            st.session_state.log = []            
         
-        st.title(self.titulo)  # Exibe o título na página
-        st.write(self.mensagem)  # Exibe uma mensagem na página
-       
         if 'pagina' not in st.session_state:
             st.session_state.pagina = 'inicial'
-            
+           
+        st.title(self.titulo)  # Exibe o título na página
+        st.write(self.mensagem)  # Exibe uma mensagem na página
+         
         self.navbar()
              
     def navbar(self):
@@ -40,26 +48,31 @@ class Aplicativo:
             self.renderiza_mqtt() 
     
     def renderiza_pagina_inicial(self):
-        st.write('Pagina inicial')
-        st.image('img.jpg', caption='Sunrise by the mountains')
+        # st.write('Pagina inicial')
+        st.image('IA URA.png')
         
     def renderiza_graficos(self):
         st.write('Página de gráficos')
-        
+   
     def renderiza_mqtt(self):
-        st.write('Página de MQTT')
-        simulador = st.button('Simula entrada de dados', use_container_width=True)
         
-        if simulador:
-            st.session_state.log.append(f'{time.strftime('%H:%M:%S')}:{random.randint(1,5)}')
-
-        if 'log' not in st.session_state:
-            st.session_state.log = []
+        try:
+        # Abre e lê o conteúdo do arquivo
+            with open(file_path, "r", encoding="utf-8") as file:
+                self.log_mqtt = file.read().split('\n')  
+                
+                # Remove o caractere '' criado na lista
+                st.session_state.log = self.log_mqtt[:-1]  
+                    
+        except FileNotFoundError:
+            st.error("Arquivo não encontrado. Verifique o caminho e tente novamente.")
             
-
+        except Exception as e:
+            st.error(f"Ocorreu um erro ao ler o arquivo: {e}")
+                    
         # Criação de colunas
         col1, col2 = st.columns([1,1])
-        
+
         with col1:
             st.header('Histórico de comandos')
             if len(st.session_state.log) > 0:
@@ -74,11 +87,13 @@ class Aplicativo:
                 st.text(st.session_state.log[-1])
             else: 
                 st.text('Nenhum comando foi enviado')
+        
+        # Atualiza a página
+        self.autorefresh = st_autorefresh(interval=2000, key="mqtt")        
 
 def main():
     # Instância da classe
     app_instance = Aplicativo(titulo='Projeto AI URA', mensagem='Colaboratividade entre carrinho e drone usando inteligência artificial')
-
     
 if __name__ == "__main__":
     main()
