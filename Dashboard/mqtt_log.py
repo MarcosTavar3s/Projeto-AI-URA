b00840cbc258bf85ea82c9f7111880496dbd6867
Marcos Aurélio Tavares Filho <86982304+MarcosTavar3s@users.noreply.github.com>
2024-11-15 15:13:11 -0300

    Update mqtt_log.py
    Uso de docstrings e typehint

--- Dashboard/mqtt_log.py
+++ Dashboard/mqtt_log.py
@@ -1,27 +1,46 @@
-# Importação das bibliotecas
 import paho.mqtt.client as mqtt
 import time
+from typing import Optional, Any
 
-# Cria o cliente do MQTT
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
 
 caminho_log = "log.txt"
 log = None
 
-# Conexão do MQTT
-def on_connect(client, userdata, flags, rc):
-    # Conectando ao broker 
+def on_connect(client: mqtt.Client, userdata: Optional[Any], flags: dict[str, Any], rc: int) -> None:
+    """
+    Função de retorno ao conectar ao broker MQTT.
+    
+    Args:
+        client: Objeto nativo do paho.mqtt.client
+        userdata: Objeto arbitrário que pode ser associado ao cliente ao criar o cliente MQTT  (padrão é None).
+        flags: Dicionário com informações adicionais sobre a conexão.
+        rc: Código de retorno que indica o resultado da tentativa de conexão.
+    
+    Retorna:
+        None.
+    """
+     
     print(f"Conectado ao broker com código de resultado {rc}")
     
-    global disconnectMqtt
-    disconnectMqtt = False
-
     # Publicando no tópico 
     client.publish('aiurarecebe/atual', 'MQTT')    
     client.subscribe("aiurarecebe/atual")
 
-# Impressão da mensagem recebida pelo tópico 
-def on_message(client, userdata, msg):
+
+def on_message(client: mqtt.Client, userdata: Any, msg: mqtt.MQTTMessage) -> None:
+    """
+    Função para ler as mensagens MQTT e gravar em um arquivo.txt.
+    
+    Args:
+        client: Objeto nativo do paho.mqtt.client.
+        userdata: Objeto arbitrário que pode ser associado ao cliente ao criar o cliente MQTT  (padrão é None).
+        msg: Um objeto do tipo MQTTMessage que contém informações sobre a mensagem recebida, como o tópico e o payload.
+        
+    Retorna:
+        None.
+    """
+    
     print(f'Mensagem de ${msg.topic}: ${msg.payload.decode()}')
      
     atual = msg.payload.decode()    
@@ -30,20 +49,36 @@ def on_message(client, userdata, msg):
         nova_mensagem = f'{time.strftime("%d/%m/%y %H:%M:%S")}: {atual}'
         arquivo.write(nova_mensagem+'\n')
 
-# Desconectar do broker
-def disconnect():
+
+def disconnect() -> None:
+    """
+    Função para desconectar do broker MQTT.
+    
+    Args:
+        None.
+            
+    Retorna:
+        None.
+    """
+    
     global client
+    
     print('Encerrando conexão com o broker...')
     client.disconnect()
     print('Conexão com o broker encerrada')
 
-# Publicar mensagem no broker
-def publish(msg):
-    global client
-    client.publish('aiurarecebe', msg)
 
-# Função principal do MQTT
-def main():        
+def main() -> None: 
+    """
+    Função principal que chama a conexão do broker e mantém a conexão viva indefinidamente.
+    
+    Args:
+        None.
+    
+    Retorna:
+        None.
+    """
+           
     global mqttMessage
     global client
 
