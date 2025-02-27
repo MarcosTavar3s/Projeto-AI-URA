0f34b9241cbb6a25728de2d8c9131cc5f7a43f1b
Marcos Aurélio Tavares Filho <86982304+MarcosTavar3s@users.noreply.github.com>
2025-02-27 17:43:48 -0300

    definicao de variaveis para alterar a quantidade de grades, cores do numero/grades e tamanho da fonte

--- Processamento de imagem/Formato de grades/quadrado.py
+++ Processamento de imagem/Formato de grades/quadrado.py
@@ -1,8 +1,9 @@
 import cv2
+import os
 
 # O codigo faz a leitura de uma imagem e salva a imagem com grades quadradas
 def main():
-  path = input("Coloque o caminho da imagem aqui:")
+  path = input("Coloque o caminho da imagem aqui: ")
   
   img = cv2.imread(path)
   
@@ -11,14 +12,23 @@ def main():
   # Quantidade de divisoes ( n x n )
   num_de_divisoes = 5
   
+  # define tamanho dos numeros na tag
+  tamanho_fonte = 1
+    
+  # cor das grades - escala bgr
+  grade_cor = (0,0,255)
+  
+  # cor dos numeros - escala bgr
+  num_cor = (255,0,0)
+  
   for i in range(1,num_de_divisoes):
-      cv2.rectangle(img, ((width//num_de_divisoes)*i, 0), ((width//num_de_divisoes)*i, height), (0,0,255), 2)
-      cv2.rectangle(img, (0, (height//num_de_divisoes)*i), (width, (height//num_de_divisoes)*i), (0,0,255), 2)
+      cv2.rectangle(img, ((width//num_de_divisoes)*i, 0), ((width//num_de_divisoes)*i, height), grade_cor, 2)
+      cv2.rectangle(img, (0, (height//num_de_divisoes)*i), (width, (height//num_de_divisoes)*i), grade_cor, 2)
   
   for i in range(1,num_de_divisoes+1):
       
       for j in range(1,num_de_divisoes+1):        
-          cv2.putText(img, str(j)+'.'+str(i), (((j*2)-1)*(width//(num_de_divisoes*2)), ((i*2)-1)*(height//(num_de_divisoes*2))), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,0,0), 2)
+          cv2.putText(img, str(j)+str(i), (((j*2)-1)*(width//(num_de_divisoes*2)), ((i*2)-1)*(height//(num_de_divisoes*2))), cv2.FONT_HERSHEY_COMPLEX_SMALL, tamanho_fonte, num_cor, 2)
   
   while True:
       cv2.imshow("Imagem", img)
@@ -28,9 +38,10 @@ def main():
       
   cv2.destroyAllWindows()
 
-  nome_arquivo = input("Coloque o nome do arquivo")
+  nome_arquivo = input("Coloque o nome do arquivo: ")
   nome_arquivo += ".jpg"
   cv2.imwrite(nome_arquivo, img)
+  print(f"Arquivo {nome_arquivo} foi salvo com sucesso na pasta {os.getcwd()}")
 
 if __name__ =="__main__":
   main()
