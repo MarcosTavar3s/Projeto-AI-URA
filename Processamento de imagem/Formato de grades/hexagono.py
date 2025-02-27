003d66d53a321d8f768a5b8149eef2ffe52481a2
Marcos Aurélio Tavares Filho <86982304+MarcosTavar3s@users.noreply.github.com>
2025-02-27 19:39:29 -0300

    inicio da implementacao hexagono.py

---
+++ Processamento de imagem/Formato de grades/hexagono.py
@@ -0,0 +1,42 @@
+import numpy as np
+import cv2
+import os
+
+# O codigo faz a leitura de uma imagem e salva a imagem com grades de hexagonos regulares
+def main():
+    path = input("Coloque o caminho da imagem aqui: ")
+    img = cv2.imread(path)
+    height, width, _ = img.shape
+              
+    # definicao do tamanho dos lados do hexagono
+    x = 10 # para a primeira linha nao violar a condicao de ser um poligono regular 
+    l = x*(2**(1/2)) # pela condicao inicial de ser regular, o lado tera esse comprimento
+    
+    # cor da grade em bgr
+    grade_cor = (0,0,255)
+    
+    # fecha o poligono
+    isClosed = True
+    
+    for i in range(int(width//l)):    
+        # coordenada dos pontos para fazer o hexagono (nao alterar)
+        pts = np.array([[x*2*i, x], [x*2*i, x+l], [x + x*2*i, 2*x+l], [2*x + x*2*i, x+l], [2*x + x*2*i, x], [x + x*2*i, 0]], np.int32)
+        pts = pts.reshape((-1, 1, 2))        
+        
+        cv2.polylines(img, [pts], isClosed, grade_cor, 2)
+    
+    while True:
+        cv2.imshow("Imagem com grades hexagonais", img)
+        
+        if cv2.waitKey(1) & 0xff == 27:
+            break
+  
+    # nome_arquivo = input("Coloque o nome do arquivo: ")
+    # nome_arquivo += ".jpg"
+    # cv2.imwrite(nome_arquivo, img)
+    # print(f"Arquivo {nome_arquivo} foi salvo com sucesso na pasta {os.getcwd()}")
+    
+    cv2.destroyAllWindows()
+    
+if __name__=="__main__":
+    main()
